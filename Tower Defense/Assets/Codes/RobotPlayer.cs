719b5ac4edf44bf5dd579de7530259a5450bf608
RGalways17 <66765133+RGalways17@users.noreply.github.com>
2020-10-18 19:12:13 +0800

    Unity Project

---
+++ Tower Defense/Assets/Codes/RobotPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotPlayer : BaseRobot
+{
+    static RobotPlayer instance;
+    public static RobotPlayer GetInstance()
+    {
+        return instance;
+    }
+    private void Awake()
+    {
+        instance = this;
+    }
+    public List<WeaponBase> Weapons;
+    public Transform Hand;
+    public float WalkSpeed = 10;
+
+    private WeaponBase CurWeapon;
+    private int CurWeaponIdx = 0;
+
+    private Animator animator;
+    private CharacterController cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var trans = Camera.main.transform;
+        var forward = Vector3.ProjectOnPlane(trans.forward, Vector3.up);
+        var right = Vector3.ProjectOnPlane(trans.right, Vector3.up);
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        var moveDirection = v * forward + h * right;
+        cc.Move(moveDirection.normalized * WalkSpeed * Time.deltaTime);
+        animator.SetFloat("Speed", cc. velocity.magnitude / WalkSpeed);
+    }
+}
