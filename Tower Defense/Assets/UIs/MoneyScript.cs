af836918126afa8a04bf919cad4908fe7c2ac241
xcjusuih <xcjusuih@gmail.com>
2020-11-22 12:11:53 +0800

    Simple UIs

---
+++ Tower Defense/Assets/UIs/MoneyScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MoneyScript : MonoBehaviour
+{
+    int money;
+    Text text;
+
+    void changeMoney(int change){
+        money+=change;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text=GetComponent<Text>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeMoney(1);
+        text.text="$"+money;
+    }
+}
---
+++ Tower Defense/Assets/UIs/TogleScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TogleScript : MonoBehaviour
+{
+    Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //找到组件，动态添加监听，Lambda表达式，精简！
+
+        GameObject.Find("Toggle").GetComponent<Toggle>().onValueChanged.AddListener(isOn => print(isOn ? "开" : "关"));
+    }
+
+    public void ListenInFunction()
+    {
+        text.fontSize=32;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
