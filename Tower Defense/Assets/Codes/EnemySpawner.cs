bee98814a8827c391af6a66865cc78a14e9e3b51
JoJoBizarreAdventure <64888038+JoJoBizarreAdventure@users.noreply.github.com>
2020-11-23 21:01:44 +0800

    monster generation script

---
+++ Tower Defense/Assets/Codes/EnemySpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Wave wave;
+    public Transform SPAWN_POINT;
+    public GameObject alive;
+    float rate;
+    // Start is called before the first frame update
+    void Start(){
+        StartCoroutine(spawnEnemy());
+    }
+    IEnumerator spawnEnemy(){
+        if(alive){
+            int cnt;
+            for (int round=0;round<wave.round;round++)
+            {
+                cnt = 0;
+                while(cnt<wave.count){
+                    for (int i=0;i<wave.limit;i++)
+                    {
+                        rate = wave.enemyPrefab[i].GetComponent<RobotEnemy>().rate;
+                        float tmp = Random.Range(0.0f,1.0f);
+                        if(tmp<rate){
+                            var enemy = GameObject.Instantiate(wave.enemyPrefab[i],SPAWN_POINT.position,Quaternion.identity);
+                            enemy.GetComponent<RobotEnemy>().target = alive;
+                            Global.enemy_count++;
+                            cnt++;
+                            yield return new WaitForSeconds(0.5f);
+                        }
+                    }
+                }
+                yield return new WaitForSeconds(wave.interval);
+            }
+        }
+    }
+}
---
+++ Tower Defense/Assets/Codes/Wave.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Wave
+{
+    public GameObject[] enemyPrefab;
+    //length of enemyPrefab
+    public int limit;
+    //防止开场出大怪
+    public int current;
+    //一轮出几个
+    public int count;
+    //有多少轮
+    public int round;
+    public float interval;
+}
