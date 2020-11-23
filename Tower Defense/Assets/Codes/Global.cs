232d49604e82d896d4ba8c2df4e2289401d8bad1
JoJoBizarreAdventure <64888038+JoJoBizarreAdventure@users.noreply.github.com>
2020-11-23 21:02:27 +0800

    global variable currently used

---
+++ Tower Defense/Assets/Codes/Global.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global
+{
+    public static int damageFactor = 1;
+    public static int money = 2000;
+    public static int enemy_count = 0;
+}
